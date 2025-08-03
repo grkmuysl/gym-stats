91a865a40b09fbf64bbbad4d43725c1aa93a8779
grkmuysl <gorkmuysal@gmail.com>
2025-08-03 15:17:41 +0300

    test

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import StackNavigation from "./src/navigation/StackNavigation";
 
