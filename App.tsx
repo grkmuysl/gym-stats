25208f49c367e7518059ec28de81e1821e08e4dd
grkmuysl <gorkmuysal@gmail.com>
2025-08-12 14:10:47 +0300

    Fix warn

--- App.tsx
+++ App.tsx
@@ -1,40 +1,60 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, UIManager, View } from "react-native";
 import StackNavigation from "./src/navigation/StackNavigation";
-import { useEffect, useState } from "react";
-import AppLoading from "expo-app-loading";
+import { useEffect, useState, useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
 import * as Font from "expo-font";
 import { FavoritesProvider } from "./src/context/FavouritesContext";
+
+if (Platform.OS === "android") {
+  if (UIManager.setLayoutAnimationEnabledExperimental) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+}
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
   const loadFonts = async () => {
-    await Font.loadAsync({
-      "Roboto-Regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
-      "Roboto-Bold": require("./src/assets/fonts/Roboto-Bold.ttf"),
-      "Roboto-Medium": require("./src/assets/fonts/Roboto-Medium.ttf"),
-      "Roboto-Light": require("./src/assets/fonts/Roboto-Light.ttf"),
-      "Roboto-SemiBold": require("./src/assets/fonts/Roboto-SemiBold.ttf"),
-    });
+    try {
+      await Font.loadAsync({
+        "Roboto-Regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
+        "Roboto-Bold": require("./src/assets/fonts/Roboto-Bold.ttf"),
+        "Roboto-Medium": require("./src/assets/fonts/Roboto-Medium.ttf"),
+        "Roboto-Light": require("./src/assets/fonts/Roboto-Light.ttf"),
+        "Roboto-SemiBold": require("./src/assets/fonts/Roboto-SemiBold.ttf"),
+      });
+    } catch (error) {
+      console.error("Font loading error:", error);
+    } finally {
+      setFontsLoaded(true);
+    }
   };
 
   useEffect(() => {
-    loadFonts()
-      .then(() => setFontsLoaded(true))
-      .catch((error) => console.error(error));
+    loadFonts();
   }, []);
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
-    return <AppLoading />;
+    return null;
   }
 
   return (
-    <FavoritesProvider>
-      <NavigationContainer>
-        <StackNavigation />
-      </NavigationContainer>
-    </FavoritesProvider>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <FavoritesProvider>
+        <NavigationContainer>
+          <StackNavigation />
+        </NavigationContainer>
+      </FavoritesProvider>
+    </View>
   );
 }
 
