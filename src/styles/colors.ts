d28ae9209d489559d4d270c06307682f6f10cce8
grkmuysl <gorkmuysal@gmail.com>
2025-09-17 12:00:51 +0300

    Color Update

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,6 +1,6 @@
 export const AppColors = {
   blackBgColor: "#161616",
-  grayBgColor: "#393939",
+  grayBgColor: "#2A2A2A",
   limeGreenColor: "#0df93e",
   whiteColor: "#ffffff",
   lightGray: "#b2afafff",
